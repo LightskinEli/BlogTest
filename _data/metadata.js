6a9776a8a5015dc429630da23a991044114764f3
LightskinEli <148436717+LightskinEli@users.noreply.github.com>
2024-04-12 13:56:07 +0100

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,13 +1,13 @@
 // update the information in this file with information about you/your site - this is for the search engine to read and display, but isn't shown on your site itself
 
 module.exports = {
-	title: "My Example Blog",
-	url: "https://example.com/",
+	title: "My Cute Blog",
+	url: "https://CuteBlog.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about how cute Hello Kitty is.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Eli",
+		email: "elirocks@example.com",
+		url: "https://elirocks.com/about-me/"
 	}
 }
